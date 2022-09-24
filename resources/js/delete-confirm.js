def752490727d561f4fc3971f9fefdf64a2cfcce
Mitsuya-Sora <three-arrows38@outlook.jp>
2022-09-24 23:48:05 +0900

    ハンバーガメニューが出ない問題を修正

--- resources/js/delete-confirm.js
+++ resources/js/delete-confirm.js
@@ -1,9 +1,10 @@
 // 削除前に確認画面を表示
 const calenderDeleteButton = document.querySelector('#calender-delete-button');
 const calenderDeleteForm = document.querySelector('#calender-delete-form');
-
-calenderDeleteButton.addEventListener('click', function() {
-    if(confirm('本当に削除しますか？')){
-        calenderDeleteForm.submit();
-    }
-});
+if(calenderDeleteButton && calenderDeleteForm){
+    calenderDeleteButton.addEventListener('click', function() {
+        if(confirm('本当に削除しますか？')){
+            calenderDeleteForm.submit();
+        }
+    });
+}
